c21fde1ba74e399d756bfb9569c888a4ba640462
a6uamr <114098029+a6uamr@users.noreply.github.com>
2024-04-14 14:14:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ document.forms[0].addEventListener('submit',e=>{e.preventDefault();
     if (!/^(?=.*[a-zA-Z])(?=.*\d)[\w@$!%*?&]{6,}$/.test(I[2].value)) {
       w('Password must be 6+ characters, 1+ letters, 1+ digits'); return;}
     U.push({n:I[0].value,e:I[1].value.toLowerCase(),p:I[2].value});
-    localStorage.setItem('U',JSON.stringify(U)); location='index.html';}
+    localStorage.setItem('U',JSON.stringify(U)); location='./';}
   else {let u; for (const e of U) if (e.e==I[0].value.toLowerCase()) {u=e; break;}
     if (!u) {w('Email not registered'); return;}
     if (u.p!=I[1].value) {w('Password incorrect'); return;}
     U[0]=u.n; localStorage.setItem('U',JSON.stringify(U));
-    location='home.html';}});
+    location='./home';}});
 dm.addEventListener('click',e=>{const D=dm.getBoundingClientRect();
   if (e.clientX<D.left||e.clientX>D.right||e.clientY<D.top||
   e.clientY>D.bottom) dm.close();});
